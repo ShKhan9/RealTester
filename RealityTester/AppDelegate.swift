b664a85eecfe41388922c0b6e941ebf732f1c742
ShehataG <shehata.g@neomtech.com>
2022-01-16 14:30:41 +0200

    First commit

---
+++ RealityTester/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  RealityTester
+//
+//  Created by Ahmed Mahmoud on 1/16/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+ 
+}
+
